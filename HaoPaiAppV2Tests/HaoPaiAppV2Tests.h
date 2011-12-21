60b0aae8621a4db775ac6b1c275f94656ef8bb49
高飞 <gaofei2003@gmail.com>
2011-12-21 22:16:26 +0800

    Initial Commit

---
+++ HaoPaiAppV2Tests/HaoPaiAppV2Tests.h
@@ -0,0 +1,13 @@
+//
+//  HaoPaiAppV2Tests.h
+//  HaoPaiAppV2Tests
+//
+//  Created by 高飞 on 11-12-21.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HaoPaiAppV2Tests : SenTestCase
+
+@end
---
+++ HaoPaiAppV2Tests/HaoPaiAppV2Tests.m
@@ -0,0 +1,32 @@
+//
+//  HaoPaiAppV2Tests.m
+//  HaoPaiAppV2Tests
+//
+//  Created by 高飞 on 11-12-21.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "HaoPaiAppV2Tests.h"
+
+@implementation HaoPaiAppV2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in HaoPaiAppV2Tests");
+}
+
+@end
